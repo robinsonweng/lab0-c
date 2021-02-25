f4187de4c198594e888372d7a45e2b1d4ad51d6c
robinsonweng <robinsonweng100@gmail.com>
2021-02-25 20:24:53 +0800

    Handle More NULL pointer from memory allocate
    If newh get NULL pointer from malloc, return NULL.
    But if newh->value get NULL pointer, shuld free newh then return NULL
    to avoid memory leaking

--- queue.c
+++ queue.c
@@ -38,12 +38,17 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return NULL;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     int slen = strlen(s);
     newh->value = (char *) malloc(sizeof(char) * slen);
+    if (!newh->value) {
+        free(newh);
+        return NULL;
+    }
     strncpy(newh->value, s, sizeof(char) * slen + 1);
     newh->next = q->head;
     q->head = newh;
